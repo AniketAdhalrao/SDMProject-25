dc296ab91c14c690c29665f6400cfa5a35648fa5
AniketAdhalrao <aniketadhalrao007@gmail.com>
2023-06-28 15:43:06 +0530

    React update

---
+++ ass1ClassComp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class HelloClassComp extends React.Component{
+    render(){
+        return <h1>Hello from React in class Component.....</h1>
+    }
+}
+
+export default HelloClassComp;
